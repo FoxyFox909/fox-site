2a17862e5cc8e2e235f956fa456102f9f4228f87
FoxyFox909 <110856509+FoxyFox909@users.noreply.github.com>
2023-02-19 22:11:59 -0500

    Add files via upload

--- assets/scripts/foxyJS.js
+++ assets/scripts/foxyJS.js
@@ -1,7 +1,43 @@
+
+function getRandomInt(max) {
+	return Math.floor(Math.random() * max);
+}
+
+
+function randomizeStart() {
+	
+	setTimeout(() => {document.getElementById('hint-button').classList.toggle('fade')}, 2000)
+	
+	const foxImagesArray = [
+	
+	'assets/images/fox-gb22281a66_1920.jpg',
+	'assets/images/49697828656_0aea963a89_k.jpg',
+	'assets/images/2530710866_f7d249a162_k.jpg',
+	'assets/images/fox-g836dca1d9_1920.jpg'
+	
+	
+	];
+	
+	var chosenFox = getRandomInt(foxImagesArray.length);
+	
+	console.log(chosenFox);
+	document.getElementById('starter-bg').src=(foxImagesArray[chosenFox])
+	
+	//setTimeout(() => {document.getElementById('starter-bg').src=(foxImagesArray[chosenFox])}, 500);
+	
+}
+
+	function openHint() {
+		document.getElementById('hint-window').classList.toggle('fade')
+	}
+
+
+
 function printFeedback(str){
-		document.getElementById('dummy').innerHTML = str
+		document.getElementById('dummy').innerHTML = str;
 	}
 	
+
 	
 
 
@@ -11,21 +47,11 @@ function nukeImages() {
 	
 	for(var i = 0; i < elementsToNuke.length; i++){
 	elementsToNuke[i].classList.add('fade');
-	}
-	
-	//document.getElementById('otocyon').classList.remove('fade');
-	
-	/*document.getElementById('nuke').classList.toggle('fade');
-	document.getElementById('otocyon').classList.toggle('fadein');*/
-	//setTimeout(function() {
-	//	document.getElementById('urocyon').classList.toggle('fadein');
-	//}, delayInMilliseconds);
-	
-	
-	
+	}		
+		
 }
 	
-//fade teh entire div then have one command to specifically bring out the desired image.
+
 
 function myFunction(){
 	let answer = document.getElementById("answer");
@@ -43,32 +69,37 @@ function myFunction(){
 			/Otocyon/i
 		];
 		
-				
+		//fade the entire div then  specifically bring out the desired image.			
 		switch (true){
 			case caseArray[0].test(answer.value): 
-				printFeedback('Correct Answer.');
+				printFeedback('That is the red fox, the most common fox.');
+				nukeImages();
+				document.getElementById('vulpes-vulpes').classList.remove('fade');
 				break;
 			case caseArray[1].test(answer.value):
-				printFeedback('That is the fox in the background, but not a red fox.');
+				printFeedback('That a snowy (arctic) fox, built for winter.');
 				nukeImages();
+				document.getElementById('vulpes-lagopus').classList.remove('fade');
 				break;
 			case caseArray[2].test(answer.value):
-				printFeedback('That is an island fox, but not a red fox.');
+				printFeedback('That is an island fox, smaller than a house cat.');
 				nukeImages();
 				document.getElementById('otocyon').classList.remove('fade');
 				break;
 			case caseArray[3].test(answer.value):
-				printFeedback('That is a bat-eared fox, but not a red fox.');
+				printFeedback('That is a bat-eared fox. The ears are radiators.');
+				nukeImages();
+				document.getElementById('urocyon').classList.remove('fade');
 				break;
 			default:
 				//document.getElementById('dummy').innerHTML = 'Wrong Answer';
-				printFeedback('Wrong answer.') 
+				printFeedback('That is not a recognized fox.') 
 		}
 		
 		//document.getElementById('dummy').innerHTML = answer.value;
 	}
 	else {
-		printFeedback('Only letters, please.');
+		printFeedback('Enter letters only, please.');
 		
 	}
 }
